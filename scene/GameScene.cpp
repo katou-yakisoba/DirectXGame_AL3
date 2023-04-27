0d0122b2aa5d109c99f0e874f49dda169059e90b
加藤　秀大 <k022g1079@g.neec.ac.jp>
2023-04-27 15:43:55 +0900

    A_02_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -2,18 +2,88 @@
 #include "TextureManager.h"
 #include <cassert>
 
+#include "MathUtilityForText.h"
+
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete spriteBG_; // BG
+
+	delete modelStage_; // ステージ
+
+	delete modelPlayer_; // プレイヤー
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	// BG(2Dスプライト)
+	textureHandleBG_ = TextureManager::Load("bg.jpg");
+	spriteBG_ = Sprite::Create(textureHandleBG_, {0, 0});
+
+	// ビュープロジェクション
+	viewProjection_.translation_.y = 1;
+	viewProjection_.translation_.z = -6;
+	viewProjection_.Initialize();
+
+	// ステージ
+	textureHandleStage_ = TextureManager::Load("stage.jpg");
+	modelStage_ = Model::Create();
+	worldTransformStage_.Initialize();
+
+	// ステージの位置を更新
+	worldTransformStage_.translation_ = {0, -1.5f, 0};
+	worldTransformStage_.scale_ = {4.5f, 1, 40};
+
+	// 変換行列を更新
+	worldTransformStage_.matWorld_ = MakeAffineMatrix(
+	    worldTransformStage_.scale_, worldTransformStage_.rotation_,
+	    worldTransformStage_.translation_);
+
+	// 変換行列を定数バッファに転送
+	worldTransformStage_.TransferMatrix();
+
+	// プレイヤー
+	textureHandlePlayer_ = TextureManager::Load("Player.png");
+	modelPlayer_ = Model::Create();
+	worldTransformPlayer_.scale_ = {0.5f, 0.5f, 0.5f};
+	worldTransformPlayer_.Initialize();
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{ PlayerUpdate(); }
+
+// プレイヤー更新
+void GameScene::PlayerUpdate() 
+{
+	// 移動
+
+	// 右へ移動
+	if (input_->PushKey(DIK_RIGHT)) {
+		worldTransformPlayer_.translation_.x += 0.1f;
+	}
+
+	// 左へ移動
+	if (input_->PushKey(DIK_LEFT)) {
+		worldTransformPlayer_.translation_.x -= 0.1f;
+	}
+
+	// 範囲制限
+
+	worldTransformPlayer_.translation_.x = max(worldTransformPlayer_.translation_.x, -4);
+	worldTransformPlayer_.translation_.x = min(worldTransformPlayer_.translation_.x, 4);
+
+	// 変換行列を更新
+	worldTransformPlayer_.matWorld_ = MakeAffineMatrix(
+		worldTransformPlayer_.scale_, 
+		worldTransformPlayer_.rotation_,
+		worldTransformPlayer_.translation_);
+	// 変換行列を定数バッファに転送
+	worldTransformPlayer_.TransferMatrix();
+}
 
 void GameScene::Draw() {
 
@@ -28,6 +98,9 @@ void GameScene::Draw() {
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
 
+	// 背景
+	spriteBG_->Draw();
+
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
@@ -41,6 +114,12 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+
+	// ステージ
+	modelStage_->Draw(worldTransformStage_, viewProjection_, textureHandleStage_);
+
+	// プレイヤー
+	modelPlayer_->Draw(worldTransformPlayer_, viewProjection_, textureHandlePlayer_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
